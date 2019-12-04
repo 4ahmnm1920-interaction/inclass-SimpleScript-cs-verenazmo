af52472eca6eeaf518a26d42c666bf5cff088d12
verenazmo <verena.zmollnig@htl-salzburg.ac.at>
2019-12-04 11:35:46 +0100

    created script

---
+++ Assets/MyGames/Scenes/Scripts/ScoreExample.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreExample : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetScore(5, "Max Muster");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetScore(78, "Verena");
+    }
+
+    public void SetScore(int score, string name)
+    {
+        int scoreValue = score;
+        string nameValue = name;
+        Debug.Log("score= " + scoreValue + " name= " + nameValue);
+    }
+}
